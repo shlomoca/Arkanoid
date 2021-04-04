af8a2cdbc6805ec138eb8825ca3e07d5e4f726d0
Daniel-Raz <dany6735@gmail.com>
2021-04-04 12:54:20 +0300

    intial commit

--- app/src/main/java/com/danielr_shlomoc/ex2/MainActivity.java
+++ app/src/main/java/com/danielr_shlomoc/ex2/MainActivity.java
@@ -3,12 +3,23 @@ package com.danielr_shlomoc.ex2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.button);
+
+        btn1.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
     }
 }
