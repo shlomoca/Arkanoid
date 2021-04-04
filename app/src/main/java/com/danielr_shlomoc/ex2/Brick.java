73329ddb0f81e03d69ddd9d7b339de2064689186
Daniel-Raz <dany6735@gmail.com>
2021-04-04 16:12:38 +0300

    bricksCollection setup

--- app/src/main/java/com/danielr_shlomoc/ex2/Brick.java
+++ app/src/main/java/com/danielr_shlomoc/ex2/Brick.java
@@ -1,4 +1,46 @@
 package com.danielr_shlomoc.ex2;
 
 public class Brick {
+
+    private float left, top,right,bottom;
+
+    public Brick(float left, float top, float right,float bottom) {
+
+        this.left = left;
+        this.top = top;
+        this.right = right;
+        this.bottom= bottom;
+    }
+
+    public float getLeft() {
+        return left;
+    }
+
+    public float getTop() {
+        return top;
+    }
+
+    public float getRight() {
+        return right;
+    }
+
+    public float getBottom() {
+        return bottom;
+    }
+
+    public void setLeft(int left) {
+        this.left = left;
+    }
+
+    public void setTop(int top) {
+        this.top = top;
+    }
+
+    public void setRight(int right) {
+        this.right = right;
+    }
+
+    public void setBottom(int bottom) {
+        this.bottom = bottom;
+    }
 }
--- app/src/main/java/com/danielr_shlomoc/ex2/GameView.java
+++ app/src/main/java/com/danielr_shlomoc/ex2/GameView.java
@@ -13,32 +13,48 @@ public class GameView extends View {
     private static final int GET_READY_STATE = 1;
     private static final int PLAYING_STATE = 2;
     private static final int GAME_OVER_STATE = 3;
-    private Paint p;
+    private final int ROWS;
+    private final int COLS;
+    private int lives, score;
+    private Paint textPaint;
+    private BrickCollection bricks;
 
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        lives = 3;
+        score = 0;
+
+        // random number in range 2-6
+        ROWS = (int) (Math.random() * 5) + 2;
+
+        // random number in range 3-7
+        COLS = (int) (Math.random() * 5) + 3;
+
+
+        // text pen
+        textPaint = new Paint();
+        textPaint.setColor(Color.BLACK);
+        textPaint.setTextSize(50);
+        textPaint.setTextAlign(Paint.Align.CENTER);
+        textPaint.setFakeBoldText(true);
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        p = new Paint();
-        p.setColor(Color.YELLOW);
-        p.setStyle(Paint.Style.FILL);
-//        canvas.drawCircle(getWidth()/2,getHeight()/2,100,p);
-        canvas.drawColor(Color.YELLOW);
+        canvas.drawColor(Color.BLACK);
+
+        bricks.drawBricks(canvas);
 
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-
+        bricks = new BrickCollection(ROWS, COLS, h, w);
     }
 
-    public void drawObj(Paint p){
 
-
-    }
 }
